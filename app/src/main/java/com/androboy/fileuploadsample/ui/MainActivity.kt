a2815fdd4436a491af4f39f441aab9350a354a2d
chandravir <chandravir.singh@mobcoder.com>
2023-12-17 15:57:02 +0530

    add click event to upload image button on main screen

--- app/src/main/java/com/androboy/fileuploadsample/ui/MainActivity.kt
+++ app/src/main/java/com/androboy/fileuploadsample/ui/MainActivity.kt
@@ -2,13 +2,17 @@ package com.androboy.fileuploadsample.ui
 
 import android.util.Log
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewbinding.ViewBinding
 import com.androboy.fileuploadsample.BaseActivity
 import com.androboy.fileuploadsample.databinding.ActivityMainBinding
+import com.androboy.fileuploadsample.ui.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
+    lateinit var ui: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
     override fun layoutRes(): ViewBinding {
         installSplashScreen()
         return ActivityMainBinding.inflate(layoutInflater)
@@ -16,6 +20,22 @@ class MainActivity : BaseActivity() {
 
     override fun initView() {
         Log.d("TAG", "initView: ")
+        ui = binding as ActivityMainBinding
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        setObservers()
+
+        setListeners()
+    }
+
+    private fun setObservers() {
+
+
+    }
+
+    private fun setListeners() {
+        ui.btnPickImage.setOnClickListener {
+
+        }
     }
 
 
