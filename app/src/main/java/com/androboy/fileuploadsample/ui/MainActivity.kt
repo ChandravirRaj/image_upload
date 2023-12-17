8b382d766c424e885fd03e302ea9d109f0ea1455
chandravir <chandravir.singh@mobcoder.com>
2023-12-17 16:19:37 +0530

    add code for bottom sheet to show image pick option

--- app/src/main/java/com/androboy/fileuploadsample/ui/MainActivity.kt
+++ app/src/main/java/com/androboy/fileuploadsample/ui/MainActivity.kt
@@ -1,29 +1,36 @@
 package com.androboy.fileuploadsample.ui
 
-import android.util.Log
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewbinding.ViewBinding
 import com.androboy.fileuploadsample.BaseActivity
 import com.androboy.fileuploadsample.databinding.ActivityMainBinding
+import com.androboy.fileuploadsample.databinding.BottomSheetCaptureBinding
 import com.androboy.fileuploadsample.ui.viewmodel.MainViewModel
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
     lateinit var ui: ActivityMainBinding
+    private lateinit var uiBottom: BottomSheetCaptureBinding
     private lateinit var viewModel: MainViewModel
+    private var mCameraSheet: BottomSheetDialog? = null
+    private var selectionCamera = 0
     override fun layoutRes(): ViewBinding {
         installSplashScreen()
         return ActivityMainBinding.inflate(layoutInflater)
     }
 
     override fun initView() {
-        Log.d("TAG", "initView: ")
         ui = binding as ActivityMainBinding
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-        setObservers()
 
+        mCameraSheet = BottomSheetDialog(this)
+        uiBottom = BottomSheetCaptureBinding.inflate(layoutInflater)
+        mCameraSheet?.setContentView(uiBottom.root)
+
+        setObservers()
         setListeners()
     }
 
@@ -34,7 +41,8 @@ class MainActivity : BaseActivity() {
 
     private fun setListeners() {
         ui.btnPickImage.setOnClickListener {
-
+            selectionCamera = 0
+            mCameraSheet?.show()
         }
     }
 
